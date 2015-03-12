1bb8d14a8d248d95b9ca00368ac6b04fe0d6f01f
Majster25 <j.halozan@gmail.com>
2015-03-12 12:09:40 +0100

    Revert "Level editor start"
    This reverts commit 2b191f3fb4cacfc3fc09255b86227bfb5996ad20.

--- levelEditorScene.cpp
+++ levelEditorScene.cpp
@@ -6,17 +6,6 @@ LevelEditorScene::LevelEditorScene(sf::RenderWindow *window, sf::Event *event, A
     this->window = window;
     this->event = event;
     next_scene = none;
-
-    const int winWidth = window->getSize().x;
-    const int winHeight = window->getSize().x;
-
-    for (unsigned short i = 0; i < kLEVEL_SIDE_DIMENSION; ++i)
-    {
-        for (unsigned short j = 0; j < kLEVEL_SIDE_DIMENSION; ++j)
-        {
-//            levelMap[i][j].renderRect.setOrigin();
-        }
-    }
 }
 
 LevelEditorScene::~LevelEditorScene()
@@ -30,27 +19,13 @@ void LevelEditorScene::handleInput()
     {
         //This should be handled everywhere
         if (event->type == sf::Event::Closed)
-            next_scene = quit;
-
-        if (event->type == sf::Event::KeyReleased)
         {
-            switch (event->key.code)
-            {
-            case sf::Keyboard::Escape:
-                next_scene = main_menu;
-                break;
-            case sf::Keyboard::C:
-                tool = kLevelEditorToolEraser;
-                break;
-            case sf::Keyboard::V:
-                tool = kLevelEditorToolWallPlacer;
-                break;
-            case sf::Keyboard::C:
-                tool = kLevelEditorToolSpecialPillPlacer;
-                break;
-            default:
-                break;
-            }
+            next_scene = quit;
+        }
+
+        if (event->type == sf::Event::KeyReleased && event->key.code == sf::Keyboard::Escape)
+        {
+            next_scene = main_menu;
         }
     }
 }
@@ -67,14 +42,6 @@ void LevelEditorScene::handleRender()
     //CRAP FONT TEST
     sf::Text text("LEVEL editor", *asset_manager->getFont());
     window->draw(text);
-
-    for (unsigned short i = 0; i < kLEVEL_SIDE_DIMENSION; ++i)
-    {
-        for (unsigned short j = 0; j < kLEVEL_SIDE_DIMENSION; ++j)
-        {
-
-        }
-    }
 
     window->display();
 }
--- levelEditorScene.h
+++ levelEditorScene.h
@@ -4,34 +4,8 @@
 #include "gameScene.h"
 #include "assetManager.h"
 
-#ifndef kLEVEL_SIDE_DIMENSION
-    #define kLEVEL_SIDE_DIMENSION 33
-#endif
-
-enum LevelEditorTool
-{
-    kLevelEditorToolEraser = 0,
-    kLevelEditorToolWallPlacer = 1,
-    kLevelEditorToolSpecialPillPlacer = 2
-};
-
-typedef enum LevelEditorTool LevelEditorTool;
-
-struct LevelTile
-{
-    sf::RectangleShape renderRect;
-    char type;
-
-};
-
-typedef struct LevelTile LevelTile;
-
 class LevelEditorScene : public GameScene
 {
-private:
-    LevelTile levelMap[kLEVEL_SIDE_DIMENSION][kLEVEL_SIDE_DIMENSION];
-    LevelEditorTool tool;
-
 public:
     LevelEditorScene(sf::RenderWindow *window, sf::Event *event, AssetManager *manager);
     virtual ~LevelEditorScene();
