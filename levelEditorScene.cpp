49bd9c3f22f05c51e9a8f89f7f14a54cfd4d8ced
Majster25 <j.halozan@gmail.com>
2015-03-12 12:10:37 +0100

    Revert "Revert "Level editor start""
    This reverts commit 1bb8d14a8d248d95b9ca00368ac6b04fe0d6f01f.

--- levelEditorScene.cpp
+++ levelEditorScene.cpp
@@ -6,6 +6,17 @@ LevelEditorScene::LevelEditorScene(sf::RenderWindow *window, sf::Event *event, A
     this->window = window;
     this->event = event;
     next_scene = none;
+
+    const int winWidth = window->getSize().x;
+    const int winHeight = window->getSize().x;
+
+    for (unsigned short i = 0; i < kLEVEL_SIDE_DIMENSION; ++i)
+    {
+        for (unsigned short j = 0; j < kLEVEL_SIDE_DIMENSION; ++j)
+        {
+//            levelMap[i][j].renderRect.setOrigin();
+        }
+    }
 }
 
 LevelEditorScene::~LevelEditorScene()
@@ -19,13 +30,27 @@ void LevelEditorScene::handleInput()
     {
         //This should be handled everywhere
         if (event->type == sf::Event::Closed)
-        {
             next_scene = quit;
-        }
 
-        if (event->type == sf::Event::KeyReleased && event->key.code == sf::Keyboard::Escape)
+        if (event->type == sf::Event::KeyReleased)
         {
-            next_scene = main_menu;
+            switch (event->key.code)
+            {
+            case sf::Keyboard::Escape:
+                next_scene = main_menu;
+                break;
+            case sf::Keyboard::C:
+                tool = kLevelEditorToolEraser;
+                break;
+            case sf::Keyboard::V:
+                tool = kLevelEditorToolWallPlacer;
+                break;
+            case sf::Keyboard::C:
+                tool = kLevelEditorToolSpecialPillPlacer;
+                break;
+            default:
+                break;
+            }
         }
     }
 }
@@ -42,6 +67,14 @@ void LevelEditorScene::handleRender()
     //CRAP FONT TEST
     sf::Text text("LEVEL editor", *asset_manager->getFont());
     window->draw(text);
+
+    for (unsigned short i = 0; i < kLEVEL_SIDE_DIMENSION; ++i)
+    {
+        for (unsigned short j = 0; j < kLEVEL_SIDE_DIMENSION; ++j)
+        {
+
+        }
+    }
 
     window->display();
 }
