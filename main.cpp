51f0088195f49b2586c9bd14e8385745f0c32c5f
Luka <redreaper132@gmail.com>
2015-03-22 14:04:00 +0100

    Disabled resizing window

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(561, 661), "Rattler Race");
+    sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(561, 661), "Rattler Race", sf::Style::Titlebar);
     sf::Event *event = new sf::Event();
 
     //Asset manager
